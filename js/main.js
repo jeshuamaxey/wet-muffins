27cd736f82fdbefc24e8312916d6dcfc4be868da
Jeshua Maxey <jeshuamaxey@gmail.com>
2013-05-10 19:49:57 +0100

    improved layout; everything is now properly centred. Added buttons to play, pause and go back to the beginning of video

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
 	//code here
+	$('.vid-controls button').click(vidAction);
 });
+
+function vidAction() {
+	console.log($(this));
+	switch($(this).attr('action')) {
+		case 'play':
+			console.log('play');
+			document.getElementById('promo-vid').play()
+			break;
+		case 'pause':
+			console.log('pause');
+			document.getElementById('promo-vid').pause()
+			break;
+		case 'restart':
+			console.log('restart');
+			document.getElementById('promo-vid').currentTime = 0;
+			break;
+	}
+}
